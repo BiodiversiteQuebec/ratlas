dba8153e5627521c4e67eecce0e9e57270211100
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2023-11-30 16:08:00 -0500

    Abstraction of url and header creation

---
+++ R/set_auth.R
@@ -0,0 +1,23 @@
+set_url <- function(endpoint) {
+
+  # Prepare HTTP request with url, header and query parameters
+  # Remove trailing slash from base_uarl if present
+  base_url <- gsub("/$", "", ATLAS_API_V2_HOST())
+
+  # Construct the URL by appending the table_name to the base URL
+  url <- paste0(base_url, "/", endpoint)
+
+  return(url)
+}
+
+set_header <- function(schema, .token = ATLAS_API_TOKEN()) {
+
+  header <- list(
+    Authorization = paste("Bearer", .token),
+    `User-Agent` = USER_AGENT(), # defined in zzz.R
+    `Content-type` = "application/json;charset=UTF-8",
+    `Accept-Profile` = schema
+  )
+
+  return(header)
+}
