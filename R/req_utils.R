70b4a22347fdad3cc71a1d220a7c37181f298fdf
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2023-12-07 15:54:31 -0500

    Added host as argument for format_url

--- R/req_utils.R
+++ R/req_utils.R
@@ -1,8 +1,8 @@
-format_url <- function(endpoint) {
+format_url <- function(endpoint, host = ATLAS_API_V2_HOST()) {
 
   # Prepare HTTP request with url, header and query parameters
   # Remove trailing slash from base_uarl if present
-  base_url <- gsub("/$", "", ATLAS_API_V2_HOST())
+  base_url <- gsub("/$", "", host)
 
   # Construct the URL by appending the table_name to the base URL
   url <- paste0(base_url, "/", endpoint)
