b97550c4e0657f4354e4e434327a0539fe57cb15
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2025-09-03 14:34:24 -0400

    remove schema public_api, as it doest exist

--- R/db_call_function.R
+++ R/db_call_function.R
@@ -1,4 +1,4 @@
-SCHEMA_VALUES <- c("public", "api", "public_api", "atlas_api")
+SCHEMA_VALUES <- c("public", "api", "atlas_api")
 
 #' Generic function to access data from functions in Atlas database
 #'
