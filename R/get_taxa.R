12d0c4907b1f8bcd0947c26123517cfd8bf7360f
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2026-06-23 11:53:33 -0400

    Fix get_taxa

--- R/get_taxa.R
+++ R/get_taxa.R
@@ -11,11 +11,14 @@
 #'
 #' @param id Optional. `integer` scalar or vector. Returns a dataframe for the
 #' taxon with the specified id
+#' @param group_name Optional. `char` scalar or vector, Returns a dataframe for
+#' the taxa belonging to the specified group(s). Must be one (or several) of the
+#' following accepted values, written exactly in uppercase:
+#' `AMPHIBIANS`, `REPTILES`, `BIRDS`, `MAMMALS`, `FISH`, `VASCULAR_PLANTS`,
+#' `NON_VASCULAR_PLANTS`, `ARTHROPODS`, `FUNGI`, `ALGAE`, `MOLLUSKS`,
+#' `OTHER_INVERTEBRATES`, `MICROORGANISMS`.
 #' @param scientific_name Optional. `char` scalar or vector. Returns a dataframe
 #' for the taxon with the specified scientific name
-#' @param match_name Optional. `char` scalar or vector. Returns a dataframe
-#' for the taxon with the specified scientific name or vernacular name.
-#' Vernacular names in french or english are accepted.
 #' @param ... Optional. scalar or vector. Returns a dataframe filtered by the
 #' atlas `taxa` table columns specified as parameter
 #'
@@ -35,9 +38,13 @@
 #' results <- get_taxa(group_fr = "Amphibiens")
 #' @export
 
+GROUPS <- c("AMPHIBIANS", "REPTILES", "BIRDS", "MAMMALS", "FISH",
+            "VASCULAR_PLANTS", "NON_VASCULAR_PLANTS", "ARTHROPODS",
+            "FUNGI", "ALGAE", "MOLLUSKS", "OTHER_INVERTEBRATES", "MICROORGANISMS")
+
 get_taxa <- function(
   id = NULL,
-  id_group = NULL,
+  group_name = NULL,
   scientific_name = NULL,
   match_name = NULL,
   ...
@@ -46,35 +53,38 @@ get_taxa <- function(
   query$table_name <- "taxa"
   query$schema <- "api"
 
-  if (! is.null(id)) {
-    query$id_taxa_obs <- id
+  if (is.null(id) && is.null(group_name) && is.null(scientific_name) && is.null(match_name)) {
+    stop("Bad input: must provide either id_taxa_obs, a group name or a scientific name")
   }
+
+  if (! is.null(id)) {
+    return(db_read_table(table_name = "taxa", schema = "api", id_taxa_obs = id))
+  }
+
+  if (! is.null(group_name)) {
+    if (!all(group_name %in% GROUPS)) stop(paste("Bad input: group_name must be one of", paste(GROUPS, collapse = ", ")))
+    match_group <- lapply(
+      group_name,
+      function(x) {
+        db_call_function(function_name = "get_taxa_from_group", schema = "api", taxa_group_short = x)
+      }
+    ) |> dplyr::bind_rows()
+    return(match_group)
+  }
+
   if (! is.null(scientific_name)) {
     match_taxa <- lapply(
       scientific_name,
       function(x) {
-        validated_name <- db_call_function(function_name = "taxa_autocomplete", schema = "atlas_api", name = scientific_name)$scientific_name
-        db_call_function("match_taxa", schema = "api", taxa_name = validated_name)
+        validated_names <- db_call_function(function_name = "taxa_autocomplete", schema = "atlas_api", name = x)
+        if (nrow(validated_names) == 0) {
+          warning(paste("No match found for", x))
+          return(NULL)
+        }
+        db_call_function("match_taxa", schema = "api", taxa_name = validated_names$scientific_name[1])
       }
     ) |> dplyr::bind_rows()
+    if (nrow(match_taxa) == 0) stop("No match found")
     return(match_taxa)
-  }
-
-  if (! is.null(match_name)) {
-    match_taxa <- lapply(
-      match_name,
-      function(x) {
-        validated_name <- db_call_function(function_name = "taxa_autocomplete", schema = "atlas_api", name = scientific_name)$scientific_name
-        db_call_function("match_taxa", schema = "api", taxa_name = validated_name)
-      }
-    ) |> dplyr::bind_rows()
-    return(match_taxa)
-  }
-  if (! is.null(id_group)) {
-    query$id_taxa_obs <- db_call_function(
-      "get_taxa_from_group",
-      schema = "api",
-      id_group = id_group
-    )$id_taxa_obs
   }
 }
