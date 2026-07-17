7ba8f2bc7ec17bdded81ad0788d5e7d4446e3fb7
BenMerSci <benjamin.b.mercier@usherbrooke.ca>
2026-07-17 17:48:32 -0400

    test(all): updated with current logic

--- tests/testthat/test-db_read_table.R
+++ tests/testthat/test-db_read_table.R
@@ -1,66 +1,58 @@
 test_that("taxa endpoint works", {
-  results <- db_read_table(table_name = "taxa")
+  results <- db_read_table(table_name = "taxa_obs")
   testthat::expect_type(results, "list")
   testthat::expect_true(nrow(results) > 100)
 })
 
 test_that("pagination works", {
-  results <- db_read_table(table_name = "taxa", .page_limit = 1000)
+  results <- db_read_table(table_name = "taxa_obs", .page_limit = 100)
   testthat::expect_true(nrow(results) > 1000)
 })
 
 test_that("no pagination on limited query", {
-  results <- db_read_table(table_name = "taxa", limit = 100)
+  results <- db_read_table(table_name = "taxa_obs", limit = 100)
   testthat::expect_true(nrow(results) == 100)
 })
 
 test_that("error on bad schema argument", {
   testthat::expect_error(
-    db_read_table(table_name = "taxa", schema = "bad_schema"),
-    regexp = "Bad input")
+                         db_read_table(table_name = "taxa", schema = "bad_schema"),
+                         regexp = "Bad input")
 })
 
 test_that("endpoint in api schema works", {
   results <- db_read_table(
-    table_name = "bird_sampling_points",
-    schema = "api",
-    limit = 100)
-  testthat::expect_true(nrow(results) == 100)
-})
-
-test_that("endpoint in api schema works", {
-  results <- db_read_table(
-    table_name = "bird_sampling_points",
-    schema = "api",
-    limit = 100)
+                           table_name = "taxa",
+                           schema = "api",
+                           limit = 100)
   testthat::expect_true(nrow(results) == 100)
 })
 
 test_that("filter by column works", {
   results <- db_read_table(
-    table_name = "taxa",
-    id = 100)
-  testthat::expect_equal(nrow(results), 1)
+                           table_name = "taxa_obs",
+                           scientific_name = "Canis lupus")
+  testthat::expect_true(nrow(results) > 0)
 
   results <- db_read_table(
-    table_name = "taxa",
-    id = c(100, 101))
-  testthat::expect_equal(nrow(results), 2)
+                           table_name = "taxa_obs",
+                           scientific_name = c("Acer saccharum", "Dryobates villosus"))
+  testthat::expect_true(nrow(results) > 0)
 })
 
 test_that("NULL parameters are not passed", {
   results <- db_read_table(
-    table_name = "observations",
-    month_obs = NULL,
-    limit=1)
+                           table_name = "observations",
+                           month_obs = NULL,
+                           limit = 1)
   testthat::expect_equal(nrow(results), 1)
 })
 
 test_that("select works", {
   results <- db_read_table(
-    table_name = "taxa",
-    select = c("id", "scientific_name"),
-    limit = 1)
+                           table_name = "taxa_obs",
+                           select = c("id", "scientific_name"),
+                           limit = 1)
   testthat::expect_equal(nrow(results), 1)
   testthat::expect_equal(ncol(results), 2)
 })
--- tests/testthat/test-get_taxa.R
+++ tests/testthat/test-get_taxa.R
@@ -1,7 +1,17 @@
-test_that("get taxa works", {
-  results <- get_taxa()
-  testthat::expect_type(results, "list")
-  testthat::expect_true(nrow(results) > 100)
+test_that("get taxa fails with no input", {
+  testthat::expect_error(get_taxa(), regexp = "Bad input")
+})
+
+test_that("get taxa fails with non accepted group_name value", {
+  testthat::expect_error(get_taxa(group_name = "Amphibiens"), regexp = "Bad input")
+})
+
+test_that("Bad name errors with no match", {
+  testthat::expect_error(get_taxa(scientific_name = "Binto"), regexp = "No match found")
+})
+
+test_that("Bad name warning with other valid names", {
+  testthat::expect_warning(get_taxa(scientific_name = c("Binto", "Dryobates villosus")), regexp = "No match found for Binto")
 })
 
 test_that("filter by id works", {
@@ -11,16 +21,9 @@ test_that("filter by id works", {
 
 test_that("filter by scientific_name works", {
   results <- get_taxa(scientific_name = "Cyanocitta cristata")
-  testthat::expect_true(nrow(results) == 1)
-  results <- get_taxa(scientific_name = c(
-    "Cyanocitta cristata",
-    "Falco peregrinus"))
+  testthat::expect_true(length(unique(results$valid_scientific_name)) == 1)
+  results <- get_taxa(scientific_name = c("Cyanocitta cristata", "Dryobates villosus"))
   testthat::expect_true(length(unique(results$valid_scientific_name)) == 2)
-})
-
-test_that("Bad name returns nothing", {
-  results <- get_taxa(scientific_name = "Binto")
-  testthat::expect_true(nrow(results) == 0)
 })
 
 test_that("Long list of `id_taxa_obs` works", {
